26a953d6eaf0546ef5c1b73b457f1d9cd716ab76
Motayassa <malovilyajob@gmail.com>
2023-10-05 17:22:28 +0300

    Add urls address "cat_by_slug" and converter slug

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),  # http://127.0.0.1:8000/
     path('cats/<int:cat_id>/', views.categories),
-    # http://127.0.0.1:8000/cats/
+    # http://127.0.0.1:8000/cats/2/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    # http://127.0.0.1:8000/cats/2fevnejvbqeo/
 ]
