80915dd741ed943dd7bc87e16561d5109d3a245c
Motayassa <malovilyajob@gmail.com>
2023-10-05 13:48:47 +0300

    Add func index in women/views.py

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):  # request это ссылка на класс HttpRequest
+    return HttpResponse("Страница приложения women.")
+    # Формирует загодовок и содержимое ответа
