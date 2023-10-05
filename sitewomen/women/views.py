f773ecc739c2510052d5a8dc280dfc96f4eff079
Motayassa <malovilyajob@gmail.com>
2023-10-05 17:25:43 +0300

    Add urls address "cat_by_slug" and converter slug

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -12,4 +12,4 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
