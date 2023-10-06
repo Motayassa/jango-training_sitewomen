4a1d3aad0581879ae195ad39e8bf43b3029a2007
Motayassa <malovilyajob@gmail.com>
2023-10-06 13:35:45 +0300

    Add name for all url address

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -6,13 +6,13 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),  # http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000/
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
     # http://127.0.0.1:8000/cats/2/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
     # http://127.0.0.1:8000/cats/2fevnejvbqeo/
     # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive)
     # регулярка, четырехзначный год
-    path("archive/<year4:year>/", views.archive)
+    path("archive/<year4:year>/", views.archive, name='archive')
     # использование самодельного конвертера
 ]
