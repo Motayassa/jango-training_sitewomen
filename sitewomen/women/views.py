ea043efdd32e47b76fefab5bdf5f50c9734e9025
Motayassa <malovilyajob@gmail.com>
2023-10-06 12:32:08 +0300

    Add condition for "page_not_found" handler for archive page

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -19,6 +19,8 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
