008ab9636da08eb0dc0571667255bf09f0efd6f8
Motayassa <malovilyajob@gmail.com>
2023-10-06 13:44:33 +0300

    Add argument in redirect for archive page

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -20,9 +20,11 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        return redirect(index, permanent=True)  # Перенаправление постоянное на главную стр
+        return redirect('cats', 'music')
+        # Перенаправление постоянное на главную стр
         # Можно использовать представления или имена
         # маршрутов в первом аргументе, вместо адреса
+        # Возможно добавление аргументов
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
