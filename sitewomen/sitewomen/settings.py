328cdc5fa1a8cffc57dc3ee2f8004293d43a9128
Motayassa <malovilyajob@gmail.com>
2023-10-08 18:02:07 +0300

    Switch on the setting DEBUG

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-au^@6m)1d9@e+3t@vs)9jo%^+6c=pvgs+*z_lv03hy3np)8q7*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
