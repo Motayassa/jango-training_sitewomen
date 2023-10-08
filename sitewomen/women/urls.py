e47e07a997fd3fa5ad00219df9b2f4231de3cac2
Motayassa <malovilyajob@gmail.com>
2023-10-08 16:50:49 +0300

    Add new views

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -8,5 +8,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000/
     path('about/', views.about, name='about'),  # http://127.0.0.1:8000/about/
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
     path('post/<int:post_id>/', views.show_post, name='post')
 ]
