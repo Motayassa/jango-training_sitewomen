7907c24547acc668242d8fc12b6dd58e21041674
Motayassa <malovilyajob@gmail.com>
2023-10-11 21:40:46 +0300

    Create tag include

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.simple_tag(name='getcats')  # создание простого тега
 def get_categories():
     return views.cats_db
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories():
+    cats = views.cats_db
+    return {'cats': cats}
+# в women/list_categories.html будет доступна переменная cats
