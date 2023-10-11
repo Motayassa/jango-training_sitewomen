6ff800d8244729928244ecc9850f03c5f6d7066d
Motayassa <malovilyajob@gmail.com>
2023-10-11 16:35:30 +0300

    Add comments

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles",  # для пожключения стат файлов к проекту
     "women.apps.WomenConfig",
 ]
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/"  # префикс для всех статических файлов в html документах
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
