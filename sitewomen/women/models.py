6ba66d47835a5b4f81062be6e75300970141eaeb
Motayassa <malovilyajob@gmail.com>
2023-10-18 18:42:36 +0300

    Fix gramma

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -5,5 +5,5 @@ class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeFields(auto_now=True)
+    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
